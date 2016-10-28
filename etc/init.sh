eebc41e8b11b27c302636c4e3c466c8207d9f944
mtirado <mtirado418@gmail.com>
2016-10-28 05:41:24 +0000

    use fstab and mtab

--- etc/init.sh
+++ etc/init.sh
@@ -5,7 +5,7 @@ IPADDR=10.0.2.15/24
 GATEWAY=10.0.2.2
 
 echo "***************************************************************"
-echo "***          super basic init script version 0.1.1          ***"
+echo "***          super basic init script version 0.2            ***"
 echo "***************************************************************"
 
 mount -vnt proc proc /proc
@@ -20,8 +20,7 @@ else
 fi
 
 # check filesystem before remounting
-# TODO test this with ext2
-# TODO add --rescue option
+# TODO add --rescue option, maybe
 if [ $RW = "no" ]; then
 	echo "checking root filesystem"
 	fsck -C -a /
@@ -41,19 +40,22 @@ if [ $RW = "no" ]; then
 	*)
 		echo "filesystem errors"
 		echo "errors are uncorrected, rebooting may fix this."
-		echo "if not, boot with --rescue appended to fix manually"
+		#echo "if not, boot with --rescue appended to fix manually"
 		echo "if ext2, \'e2fsck -v -y <root-partition>\' might help."
 		echo "an incorrect /etc/fstab can also cause this error."
 	;;
 	esac
 	if [ $CHKRET -ge 2 ]; then
-		echo "press any key to reboot"
-		read -n 1 anykey
 		umount -var
 		umount -vn -o remount,ro /
-		echo "rebooting..."
-		sleep 1
-		reboot -f
+		echo "reboot now"
+		read -n 1 anykey
+		read -n 1 anykey
+		read -n 1 anykey
+		read -n 1 anykey
+		# TODO call reboot and test this stuff again
+		exit -1
+		#reboot -f
 	fi
 else
 	echo "filesystem was already mounted in r/w mode, this is unexpected."
@@ -64,12 +66,10 @@ fi
 
 
 # remount r+w
-echo "remounting root filesystem in read/write mode"
 mount -wvn -o remount /
 if [ $? -eq 0 ]; then
 	rm -f /etc/mtab
-	mount -vt proc proc /proc
-	mount -vt sysfs sysfs /sys
+	echo "remounted rootfs with +rw"
 else
 	echo "failed remounting root filesystem in read/write mode"
 	echo "press any key to continue"
@@ -80,23 +80,32 @@ fi
 # devices
 # mount dev as tmpfs?
 chown -R 0:0 /dev
-chown 0:tty /dev/{ptmx,tty}
+chown 0:0 /dev/{ptmx,tty}
 chown 0:0 /dev/console
 
+mkdir -p /dev/pts
+mkdir -p /home
+mkdir -p /opt
 
-# filesystems
-# TODO mount stuff in fstab
-mkdir /dev/pts
-mount -vt devpts -o gid=5,mode=0620 devpts /dev/pts
+mount -wv -o remount /
+mount -v -a
+if [ $? -ne 0 ]; then
+	echo "fstab mount issues <-------------"
+fi
 
 #sysv shm
-echo "set ip address: $IPADDR"
+
+#setup network
+echo "setting ip address: $IPADDR"
 ip addr add $IPADDR dev eth0
 echo "bringing up $NETDEV"
 ip link set dev $NETDEV up
-echo "set gateway: $GATEWAY"
+echo "setting gateway: $GATEWAY"
 ip route add default via $GATEWAY
-
+/root/firewall.sh
+if [ $? -ne 0 ]; then
+	echo "firewall script failed! <-------------"
+fi
 exit 0
 
 
